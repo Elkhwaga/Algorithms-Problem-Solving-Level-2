d7e029c554f96cd5fa04bdcc19c0809b46cf9fda
Elkhwaga <mohamedelkhwaga9@gmail.com>
2025-03-11 05:01:32 +0200

    my ceil function

--- 49_my_ceil.cpp
+++ 49_my_ceil.cpp
@@ -1,15 +1,35 @@
-#include <iostream>
-#include <cmath>
+#include "./lib/input_number.h"
+#include "./lib/display.h"
 
-using namespace std;
+/*
+    ==============================================
+    |    Algorithms & Problem-Solving Level 2    |
+    ==============================================
+    |                                            |
+    |       @Author: Firstname Lastname          |
+    |       @version 1.0                         |
+    |                                            |
+    ==============================================
 
-float readNumber()
-{
-    float number;
-    cout << "Enter a number: ";
-    cin >> number;
-    return number;
-}
+
+    - Create a function that takes a number as a parameter and returns the ceiling value of the number.
+        -- The function should use a conditional statement to check if the number is positive or negative and return the ceiling value of the number.
+
+    - #include "./lib/input_number.h" -> InputNumber::readNumber
+    - #include "./lib/display.h" -> Display::displayWelcomeMessage
+    - #include "./lib/display.h" -> Display::displayGoodbyeMessage
+
+    ---------------------------------------------------------------------------
+    - Example Input:
+        Enter a number: 5.5
+
+    ---------------------------------------------------------------------------
+    - Example Output:
+        My Ceil: 6
+        c++ ceil: 6
+
+    -- Goodbye!
+*/
 
 float getFractionPart(float number)
 {
@@ -27,9 +47,13 @@ int myCeil(float number)
 
 int main()
 {
-    float number = readNumber();
-    cout << "My Ceil: " << myCeil(number) << endl;
-    cout << "c++ ceil: " << ceil(number) << endl;
+    Display::displayWelcomeMessage("Welcome To My Ceil");
+
+    float number = InputNumber::readNumber("Enter a number: ");
+    std::cout << "My Ceil: " << myCeil(number) << std::endl;
+    std::cout << "c++ ceil: " << ceil(number) << std::endl;
+
+    Display::displayGoodbyeMessage("Goodbye!");
 
     return 0;
 }
