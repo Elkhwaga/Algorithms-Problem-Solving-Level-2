3f0b0c7adbf0549a7d964c52260d95f56d3bfcda
Elkhwaga <mohamedelkhwaga9@gmail.com>
2025-03-11 04:28:41 +0200

    copy distinct numbers to array

--- 40_copy_distinct_numbers_to_array.cpp
+++ 40_copy_distinct_numbers_to_array.cpp
@@ -5,6 +5,16 @@
 #include "./lib/display.h"
 
 /*
+    ==============================================
+    |    Algorithms & Problem-Solving Level 2    |
+    ==============================================
+    |                                            |
+    |       @Author: Firstname Lastname          |
+    |       @version 1.0                         |
+    |                                            |
+    ==============================================
+
+
     - Create a function that takes two arrays and their sizes as parameters and copies the distinct numbers from the first array into the second array.
         -- The function should use a loop to iterate through the first array and copy the distinct numbers into the second array.
 
