b436c994b17e9864937113cf95fd9fe300c2d44e
Elkhwaga <mohamedelkhwaga9@gmail.com>
2025-03-11 05:00:39 +0200

    my abs function

--- 46_my_abs.cpp
+++ 46_my_abs.cpp
@@ -1,14 +1,35 @@
-#include <iostream>
+#include "./lib/input_number.h"
+#include "./lib/display.h"
 
-using namespace std;
+/*
+    ==============================================
+    |    Algorithms & Problem-Solving Level 2    |
+    ==============================================
+    |                                            |
+    |       @Author: Firstname Lastname          |
+    |       @version 1.0                         |
+    |                                            |
+    ==============================================
 
-float readNumber()
-{
-    float number;
-    cout << "Enter a number: ";
-    cin >> number;
-    return number;
-}
+
+    - Create a function that takes a number as a parameter and returns the absolute value of the number.
+        -- The function should use a conditional statement to check if the number is positive or negative and return the absolute value of the number.
+
+    - #include "./lib/input_number.h" -> InputNumber::readNumber
+    - #include "./lib/display.h" -> Display::displayWelcomeMessage
+    - #include "./lib/display.h" -> Display::displayGoodbyeMessage
+
+    ---------------------------------------------------------------------------
+    - Example Input:
+        Enter a number: 5
+
+    ---------------------------------------------------------------------------
+    - Example Output:
+        My ABS: 5
+        c++ abs: 5
+
+    -- Goodbye!
+*/
 
 float myABS(float number)
 {
@@ -17,9 +38,13 @@ float myABS(float number)
 
 int main()
 {
-    float number = readNumber();
-    cout << "My ABS: " << myABS(number) << endl;
-    cout << "c++ abs: " << abs(number) << endl;
+    Display::displayWelcomeMessage("Welcome To My ABS");
+
+    float number = InputNumber::readNumber("Enter a number: ");
+    std::cout << "My ABS: " << myABS(number) << std::endl;
+    std::cout << "c++ abs: " << abs(number) << std::endl;
+
+    Display::displayGoodbyeMessage("Goodbye!");
 
     return 0;
 }
