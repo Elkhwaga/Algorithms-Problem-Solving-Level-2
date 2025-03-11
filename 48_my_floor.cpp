f7333eb4f07ae5c7d859a74ccc7b13f0368f76b0
Elkhwaga <mohamedelkhwaga9@gmail.com>
2025-03-11 05:01:12 +0200

    my floor function

--- 48_my_floor.cpp
+++ 48_my_floor.cpp
@@ -1,15 +1,35 @@
-#include <iostream>
-#include <cmath>
+#include "./lib/input_number.h"
+#include "./lib/display.h"
 
-using namespace std;
+/*
+    ==============================================
+    |    Algorithms & Problem-Solving Level 2    |
+    ==============================================
+    |                                            |
+    |       @Author: Firstname Lastname          |
+    |       @version 1.0                         |
+    |                                            |
+    ==============================================
 
-float readNumber()
-{
-    float number;
-    cout << "Enter a number: ";
-    cin >> number;
-    return number;
-}
+
+    - Create a function that takes a number as a parameter and returns the floor value of the number.
+        -- The function should use a conditional statement to check if the number is positive or negative and return the floor value of the number.
+
+    - #include "./lib/input_number.h" -> InputNumber::readNumber
+    - #include "./lib/display.h" -> Display::displayWelcomeMessage
+    - #include "./lib/display.h" -> Display::displayGoodbyeMessage
+
+    ---------------------------------------------------------------------------
+    - Example Input:
+        Enter a number: 5.5
+
+    ---------------------------------------------------------------------------
+    - Example Output:
+        My Floor: 5
+        c++ floor: 5
+
+    -- Goodbye!
+*/
 
 int myFloor(float number)
 {
@@ -18,9 +38,13 @@ int myFloor(float number)
 
 int main()
 {
-    float number = readNumber();
-    cout << "My Floor: " << myFloor(number) << endl;
-    cout << "c++ floor: " << floor(number) << endl;
+    Display::displayWelcomeMessage("Welcome To My Floor");
+
+    float number = InputNumber::readNumber("Enter a number: ");
+    std::cout << "My Floor: " << myFloor(number) << std::endl;
+    std::cout << "c++ floor: " << floor(number) << std::endl;
+
+    Display::displayGoodbyeMessage("Goodbye!");
 
     return 0;
 }
