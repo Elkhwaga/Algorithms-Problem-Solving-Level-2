d69fc88e66a104cfc213c36d5ac08d0bb582130f
Elkhwaga <mohamedelkhwaga9@gmail.com>
2025-03-11 05:00:56 +0200

    my round function

--- 47_my_round.cpp
+++ 47_my_round.cpp
@@ -1,15 +1,35 @@
-#include <iostream>
-#include <cmath>
+#include "./lib/input_number.h"
+#include "./lib/display.h"
 
-using namespace std;
+/*
+    ==============================================
+    |    Algorithms & Problem-Solving Level 2    |
+    ==============================================
+    |                                            |
+    |       @Author: Firstname Lastname          |
+    |       @version 1.0                         |
+    |                                            |
+    ==============================================
 
-float readNumber()
-{
-    float number;
-    cout << "Enter a number: ";
-    cin >> number;
-    return number;
-}
+
+    - Create a function that takes a number as a parameter and returns the rounded value of the number.
+        -- The function should use the round function to round the number to the nearest integer.
+
+    - #include "./lib/input_number.h" -> InputNumber::readNumber
+    - #include "./lib/display.h" -> Display::displayWelcomeMessage
+    - #include "./lib/display.h" -> Display::displayGoodbyeMessage
+
+    ---------------------------------------------------------------------------
+    - Example Input:
+        Enter a number: 5.5
+
+    ---------------------------------------------------------------------------
+    - Example Output:
+        My Round: 6
+        c++ round: 6
+
+    -- Goodbye!
+*/
 
 float getFractionPart(float number)
 {
@@ -28,9 +48,13 @@ int myRound(float number)
 
 int main()
 {
-    float number = readNumber();
-    cout << "My Round: " << myRound(number) << endl;
-    cout << "c++ round: " << round(number) << endl;
+    Display::displayWelcomeMessage("Welcome To My Round");
+
+    float number = InputNumber::readNumber("Enter a number: ");
+    std::cout << "My Round: " << myRound(number) << std::endl;
+    std::cout << "c++ round: " << round(number) << std::endl;
+
+    Display::displayGoodbyeMessage("Goodbye!");
 
     return 0;
 }
