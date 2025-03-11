afef8be378afbda3338c8046707cd4af9c39bf15
Elkhwaga <mohamedelkhwaga9@gmail.com>
2025-03-11 05:01:47 +0200

    my sqrt function

--- 50_my_sqrt.cpp
+++ 50_my_sqrt.cpp
@@ -1,15 +1,35 @@
-#include <iostream>
-#include <cmath>
+#include "./lib/input_number.h"
+#include "./lib/display.h"
 
-using namespace std;
+/*
+    ==============================================
+    |    Algorithms & Problem-Solving Level 2    |
+    ==============================================
+    |                                            |
+    |       @Author: Firstname Lastname          |
+    |       @version 1.0                         |
+    |                                            |
+    ==============================================
 
-float readNumber()
-{
-    float number;
-    cout << "Enter a number: ";
-    cin >> number;
-    return number;
-}
+
+    - Create a function that takes a number as a parameter and returns the square root of the number.
+        -- The function should use the pow function to calculate the square root of the number.
+
+    - #include "./lib/input_number.h" -> InputNumber::readNumber
+    - #include "./lib/display.h" -> Display::displayWelcomeMessage
+    - #include "./lib/display.h" -> Display::displayGoodbyeMessage
+
+    ---------------------------------------------------------------------------
+    - Example Input:
+        Enter a number: 25
+
+    ---------------------------------------------------------------------------
+    - Example Output:
+        My Sqrt: 5
+        c++ sqrt: 5
+
+    -- Goodbye!
+*/
 
 int mySqrt(float number)
 {
@@ -18,9 +38,13 @@ int mySqrt(float number)
 
 int main()
 {
-    float number = readNumber();
-    cout << "My Sqrt: " << mySqrt(number) << endl;
-    cout << "c++ sqrt: " << sqrt(number) << endl;
+    Display::displayWelcomeMessage("Welcome To My Sqrt");
+
+    float number = InputNumber::readNumber("Enter a number: ");
+    std::cout << "My Sqrt: " << mySqrt(number) << std::endl;
+    std::cout << "c++ sqrt: " << sqrt(number) << std::endl;
+
+    Display::displayGoodbyeMessage("Goodbye!");
 
     return 0;
 }
