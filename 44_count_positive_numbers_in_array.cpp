eb89455ca585a0e2855ea2b05c162f6e0e99881e
Elkhwaga <mohamedelkhwaga9@gmail.com>
2025-03-11 04:49:26 +0200

    count positive numbers in array

--- 44_count_positive_numbers_in_array.cpp
+++ 44_count_positive_numbers_in_array.cpp
@@ -1,85 +1,68 @@
-#include <iostream>
-#include <limits>
-#include <cstdlib>
+#include "./lib/input_number.h"
+#include "./lib/array.h"
+#include "./lib/check.h"
+#include "./lib/display.h"
 
-using namespace std;
+/*
+    ==============================================
+    |    Algorithms & Problem-Solving Level 2    |
+    ==============================================
+    |                                            |
+    |       @Author: Firstname Lastname          |
+    |       @version 1.0                         |
+    |                                            |
+    ==============================================
 
-enum PositiveOrNegative
-{
-    Positive = 1,
-    Negative = 2
-};
 
-int randomNumber(int min, int max)
-{
-    return rand() % (max - min + 1) + min;
-}
+    - Create a function that takes an array and its length as parameters and returns the count of positive numbers in the array.
+        -- The function should use a loop to iterate through the array and check if each element is positive.
+        -- The function should return the count of positive numbers in the array.
 
-int readPositiveNumber(const string &message, int min, int max)
-{
-    int number = 0;
-    do
-    {
-        cout << message;
-        cin >> number;
-        if (cin.fail() || number < min || number > max)
-        {                                                        // Check if the input is valid and non-negative
-            cin.clear();                                         // Reset the input stream in case of an error
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignore invalid input
-            cout << "Invalid input. Please enter a non-negative integer between " << min << " and " << max << endl;
-            number = -1; // Prompt the user again
-        }
-    } while (number < min || number > max); // Continue until a valid positive integer is entered
-    return number;
-}
+    - #include "./lib/input_number.h"   -> InputNumber::readNumberInRange
+    - #include "./lib/array.h"          -> Array::createArrayRandom
+    - #include "./lib/array.h"          -> Array::printArray
+    - #include "./lib/check.h"           -> Check::getNumberTypeNegativeOrPositive
+    - #include "./lib/display.h"        -> Display::displayWelcomeMessage
+    - #include "./lib/display.h"        -> Display::displayGoodbyeMessage
 
-PositiveOrNegative getNumberType(int number)
-{
-    return number >= 0 ? PositiveOrNegative::Positive : PositiveOrNegative::Negative;
-}
+    ---------------------------------------------------------------------------
+    - Example Input:
+        Enter the length of the array: 5
 
-void createArray(int arr[], int length)
-{
-    for (int i = 0; i < length; i++)
-    {
-        arr[i] = randomNumber(-100, 100);
-    }
-}
+    ---------------------------------------------------------------------------
+    - Example Output:
+        Original Array: 5 2 59 70 -29
+        positive number count is: 4
+
+    -- Goodbye!
+*/
 
 int countPositiveNumbersInArray(int arr[], int length)
 {
     int count = 0;
     for (int i = 0; i < length; i++)
     {
-        if (getNumberType(arr[i]) == PositiveOrNegative::Positive)
+        if (Check::getNumberTypeNegativeOrPositive(arr[i]) == Enums::PositiveOrNegative::POSITIVE)
             count++;
     }
     return count;
 }
 
-void printArray(int arr[], int length, string const &message)
-{
-    cout << message;
-    if (length == 0)
-    {
-        cout << "Empty Array" << endl;
-        return;
-    }
-    for (int i = 0; i < length; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
-
 int main()
 {
-    srand((unsigned)time(NULL));
-    int arr[100],
-        length = readPositiveNumber("Enter the length of the array: ", 1, 100);
-    createArray(arr, length);
+    Display::displayWelcomeMessage("Welcome To Count Positive Numbers In Array");
 
-    printArray(arr, length, "Original Array: ");
-    cout << "positive number count is: " << countPositiveNumbersInArray(arr, length);
+    srand((unsigned)time(NULL));
+
+    int arr[100],
+        length = InputNumber::readNumberCustom("Enter the length of the array: ", false);
+
+    Array::createArrayRandom(arr, length);
+    Array::printArray(arr, length, "Original Array: ");
+
+    std::cout << "positive number count is: " << countPositiveNumbersInArray(arr, length) << std::endl;
+
+    Display::displayGoodbyeMessage("Goodbye!");
+
     return 0;
 }
